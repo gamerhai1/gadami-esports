9d9ad8685c5e2d032e3e3435e00561ee9056a3fa
ESports <gamergadami@gmail.com>
2025-06-19 16:43:00 +0530

    Update and rename App.jsx to src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold text-green-400">Welcome to Gadami eSports</h1>
+    </div>
+  )
+}
