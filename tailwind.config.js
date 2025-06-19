925aed4d2ab02cc8c7fec9ee3296d930f9dbd4df
ESports <gamergadami@gmail.com>
2025-06-19 16:33:05 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx}"
   ],
   theme: {
-    extend: {
-      colors: {
-        neon: "#39ff14",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
